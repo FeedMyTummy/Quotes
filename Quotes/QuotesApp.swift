a8c9175c37196f405112b94c542dfca5456c4c02
FeedMyTummy <21245786+FeedMyTummy@users.noreply.github.com>
2020-11-06 15:47:58 -0500

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by FeedMyTummy on 11/6/20.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
